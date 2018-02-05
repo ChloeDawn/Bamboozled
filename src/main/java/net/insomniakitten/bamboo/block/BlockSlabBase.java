6334f84e9767ef5cc96bb6d00c1bdd7f25301f74
InsomniaKitten <admin@designdevils.co.uk>
2018-02-05 00:07:11 +0000

    Adapt slab implementation to not extend BlockSlab

--- src/main/java/net/insomniakitten/bamboo/block/BlockSlabBase.java
+++ src/main/java/net/insomniakitten/bamboo/block/BlockSlabBase.java
@@ -1,18 +1,15 @@
 package net.insomniakitten.bamboo.block;
 
-import net.insomniakitten.bamboo.Bamboozled;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockSlab;
+import net.minecraft.block.BlockSlab.EnumBlockHalf;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.IStringSerializable;
@@ -28,23 +25,20 @@ import java.util.Locale;
 import java.util.Random;
 import java.util.function.Predicate;
 
-public abstract class BlockSlabBase extends BlockSlab {
+public abstract class BlockSlabBase extends BlockBase {
 
     public static final PropertyEnum<Variant> VARIANT = PropertyEnum.create("variant", Variant.class);
 
-    private String name;
-
     public BlockSlabBase(Material material, MapColor mapColor, SoundType sound, float hardness, float resistance) {
-        super(material, mapColor);
-        setHardness(hardness);
-        setResistance(resistance);
-        setSoundType(sound);
-        setCreativeTab(Bamboozled.TAB);
+        super(material, mapColor, sound, hardness, resistance);
+        useNeighborBrightness = true;
         setLightOpacity(0);
     }
 
     public BlockSlabBase(Material material, SoundType sound, float hardness, float resistance) {
-        this(material, material.getMaterialMapColor(), sound, hardness, resistance);
+        super(material, sound, hardness, resistance);
+        useNeighborBrightness = true;
+        setLightOpacity(0);
     }
 
     public IBlockState getLower() {
@@ -75,47 +69,28 @@ public abstract class BlockSlabBase extends BlockSlab {
     }
 
     @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return state.getValue(VARIANT).getBoundingBox();
-    }
-
-    @Override
+    @Deprecated
     public boolean isTopSolid(IBlockState state) {
         return isDouble(state) || isUpper(state);
     }
 
     @Override
     @Deprecated
-    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing side) {
-        return state.getValue(VARIANT).isSideSolid(side) ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
+    public IBlockState getStateFromMeta(int meta) {
+        Variant variant = Variant.VALUES[meta];
+        return getDefaultState().withProperty(VARIANT, variant);
     }
 
-    @Override
     @Deprecated
-    public boolean isOpaqueCube(IBlockState state) {
-        return isDouble(state);
-    }
-
-    @Override
-    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
-        return !ForgeModContainer.disableStairSlabCulling && state.getValue(VARIANT).isSideSolid(side);
-    }
-
-    @Override
-    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return getStateForPlacement(world, pos, side, hitX, hitY, hitZ, meta, placer, placer.getActiveHand());
-    }
-
-    @Override
-    @Deprecated
-    public int quantityDropped(Random random) {
-        return 1;
-    }
-
-    @Override
-    @Deprecated
-    public boolean isFullCube(IBlockState state) {
-        return isDouble(state);
+    @SideOnly(Side.CLIENT)
+    public int getPackedLightmapCoords(IBlockState state, IBlockAccess world, BlockPos pos) {
+        int light = world.getCombinedLight(pos, state.getLightValue(world, pos));
+        if (light == 0) {
+            pos = pos.down();
+            state = world.getBlockState(pos);
+            return world.getCombinedLight(pos, state.getLightValue(world, pos));
+        }
+        return light;
     }
 
     @Override
@@ -138,57 +113,31 @@ public abstract class BlockSlabBase extends BlockSlab {
     }
 
     @Override
-    public String getUnlocalizedName(int meta) {
-        return getUnlocalizedName();
-    }
-
-    @Override
-    @Deprecated // Use state-sensitive version
-    public boolean isDouble() {
-        return false;
-    }
-
-    @Override
-    @Deprecated // Only used by ItemSlab
-    public IProperty<?> getVariantProperty() {
-        return VARIANT;
-    }
-
-    @Override
-    @Deprecated // Only used by ItemSlab
-    public Comparable<?> getTypeForItem(ItemStack stack) {
-        return o -> 0;
+    @Deprecated
+    public int quantityDropped(Random random) {
+        return 1;
     }
 
     @Override
     @Deprecated
-    public IBlockState getStateFromMeta(int meta) {
-        Variant variant = Variant.VALUES[meta];
-        return getDefaultState().withProperty(VARIANT, variant);
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return getStateForPlacement(world, pos, side, hitX, hitY, hitZ, meta, placer, placer.getActiveHand());
     }
 
     @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(VARIANT).ordinal();
-    }
-
-    @Override
-    public final Block setUnlocalizedName(String name) {
-        return super.setUnlocalizedName(Bamboozled.ID + "." + name);
-    }
-
-    @Override
-    public final String getUnlocalizedName() {
-        if (name == null) {
-            name = super.getUnlocalizedName()
-                    .replace("tile.", "block.");
-        }
-        return name;
+    @Deprecated
+    protected boolean canSilkHarvest() {
+        return false;
     }
 
     @Override
     protected BlockStateContainer createBlockState() {
         return new BlockStateContainer(this, VARIANT);
+    }
+
+    @Override
+    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return !ForgeModContainer.disableStairSlabCulling && state.getValue(VARIANT).isSideSolid(side);
     }
 
     @Override
@@ -201,6 +150,35 @@ public abstract class BlockSlabBase extends BlockSlab {
         if (side == EnumFacing.UP) return getLower();
         if (side == EnumFacing.DOWN) return getUpper();
         return hitY > 0.5D ? getUpper() : getLower();
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(VARIANT).ordinal();
+    }
+
+    @Override
+    @Deprecated
+    public boolean isFullCube(IBlockState state) {
+        return isDouble(state);
+    }
+
+    @Override
+    @Deprecated
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return state.getValue(VARIANT).getBoundingBox();
+    }
+
+    @Override
+    @Deprecated
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing side) {
+        return state.getValue(VARIANT).isSideSolid(side) ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
+    }
+
+    @Override
+    @Deprecated
+    public boolean isOpaqueCube(IBlockState state) {
+        return isDouble(state);
     }
 
     public enum Variant implements IStringSerializable {
