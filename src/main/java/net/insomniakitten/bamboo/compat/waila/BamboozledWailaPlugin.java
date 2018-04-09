fdbb841ebc321cab2593d695e3d7f3c1d159db9e
InsomniaKitten <admin@designdevils.co.uk>
2018-04-09 15:36:33 +0100

    Inline Waila data provider

--- src/main/java/net/insomniakitten/bamboo/compat/waila/BamboozledWailaPlugin.java
+++ src/main/java/net/insomniakitten/bamboo/compat/waila/BamboozledWailaPlugin.java
@@ -1,19 +1,44 @@
 package net.insomniakitten.bamboo.compat.waila;
 
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import mcp.mobius.waila.api.WailaPlugin;
 import net.insomniakitten.bamboo.Bamboozled;
+import net.insomniakitten.bamboo.BamboozledBlocks;
 import net.insomniakitten.bamboo.block.BlockBambooBundle;
-import net.insomniakitten.bamboo.compat.waila.provider.ProviderBambooBundle;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 @WailaPlugin
 public final class BamboozledWailaPlugin implements IWailaPlugin {
 
+    public static final IWailaDataProvider BAMBOO_BUNDLE_PROVIDER = new IWailaDataProvider() {
+        @Override
+        @SideOnly(Side.CLIENT)
+        public List<String> getWailaBody(ItemStack stack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+            final BlockBambooBundle bundle = (BlockBambooBundle) BamboozledBlocks.BAMBOO_BUNDLE;
+            if (bundle.isDryingEnabled()) {
+                final int progress = bundle.getDryProgress(accessor.getBlockState());
+                final String key = "waila.bamboozled.bamboo_bundle.dry_progress";
+                if (progress < 3 && I18n.hasKey(key)) {
+                    tooltip.add(I18n.format(key, 33 * progress));
+                }
+            }
+            return tooltip;
+        }
+    };
+
     @Override
     public void register(IWailaRegistrar registrar) {
         Bamboozled.LOGGER.debug("Registering Hwyla data providers...");
-        registrar.registerBodyProvider(ProviderBambooBundle.INSTANCE, BlockBambooBundle.class);
+        registrar.registerBodyProvider(BAMBOO_BUNDLE_PROVIDER, BlockBambooBundle.class);
     }
 
 }
