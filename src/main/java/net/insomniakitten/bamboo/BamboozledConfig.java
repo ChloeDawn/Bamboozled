b4db3acfc9f8731bb664bf446376eed40ba7184f
InsomniaKitten <admin@designdevils.co.uk>
2018-06-05 17:46:00 +0100

    Fix configuration not generation TODO: Custom implementation for encapsulation

--- src/main/java/net/insomniakitten/bamboo/BamboozledConfig.java
+++ src/main/java/net/insomniakitten/bamboo/BamboozledConfig.java
@@ -17,12 +17,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @EventBusSubscriber(modid = Bamboozled.ID)
 @Config(modid = Bamboozled.ID, name = Bamboozled.ID, category = "")
 public class BamboozledConfig {
-    protected final Client CLIENT = new Client();
-    protected final General GENERAL = new General();
-    protected final World WORLD = new World();
+    public final Client CLIENT = new Client();
+    public final General GENERAL = new General();
+    public final World WORLD = new World();
 
     @SubscribeEvent
-    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+    protected void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
         if (Bamboozled.ID.equals(event.getModID())) {
             val lastFancySaltOre = CLIENT.isFancySaltOreForced();
             ConfigManager.sync(Bamboozled.ID, Config.Type.INSTANCE);
@@ -38,7 +38,7 @@ public class BamboozledConfig {
         @Name("force_fancy_salt_ore")
         @Comment({ "Should halite always render as a translucent block?",
                    "If false, halite will render solid on Fast graphics." })
-        private boolean fancySaltOreForced = false;
+        public boolean fancySaltOreForced = false;
     }
 
     @Data
@@ -46,35 +46,35 @@ public class BamboozledConfig {
         @Name("in_world_bamboo_drying")
         @Comment("Should bundles of bamboo dry out over time when placed outside under the sun?")
         @RequiresMcRestart
-        private boolean inWorldBambooDryingEnabled = true;
+        public boolean inWorldBambooDryingEnabled = true;
 
         @Name("salt_block_drops_itself")
         @Comment("Should salt blocks drop themselves when broken? If false, they will drop 9 salt piles")
         @RequiresMcRestart
-        private boolean saltBlockDropsEnabled = false;
+        public boolean saltBlockDropsEnabled = false;
 
         @Name("salt_hurts_undead")
         @Comment("Should salt hurt undead mobs that walk on it?")
         @RequiresMcRestart
-        private boolean saltUndeadDamageEnabled = true;
+        public boolean saltUndeadDamageEnabled = true;
 
         @Name("throwable_salt_piles")
         @Comment({ "Should piles of salt be throwable?",
                    "If \"salt_hurts_undead\" is enabled, thrown salt will also deal damage" })
         @RequiresMcRestart
-        private boolean throwableSaltPilesEnabled = true;
+        public boolean throwableSaltPilesEnabled = true;
 
         @Name("throw_requires_sneaking")
         @Comment({ "Should sneaking be required to throw salt piles?",
                    "This config is unused if \"throwable_salt_piles\" is disabled" })
         @RequiresMcRestart
-        private boolean throwSneakingRequirementEnabled = false;
+        public boolean throwSneakingRequirementEnabled = false;
 
         @Name("fancy_bamboo")
         @Comment({ "Should the bounding box of bamboo be fancy and detailed?",
                    "When false, collision logic will also be simplified" })
         @RequiresMcRestart
-        private boolean fancyBambooEnabled = true;
+        public boolean fancyBambooEnabled = true;
     }
 
     @Data
@@ -82,16 +82,16 @@ public class BamboozledConfig {
         @Name("generate_bamboo")
         @Comment("Should bamboo stalks be generated in tropical biomes?")
         @RequiresMcRestart
-        private boolean bambooGenerationEnabled = true;
+        public boolean bambooGenerationEnabled = true;
 
         @Name("generate_salt_ore")
         @Comment("Should halite clusters be generated underground?")
         @RequiresMcRestart
-        private boolean saltOreGenerationEnabled = true;
+        public boolean saltOreGenerationEnabled = true;
 
         @Name("salt_ore_cluster_size")
         @Comment("The size of generated halite clusters")
         @RequiresMcRestart
-        private int saltClusterSize = 8;
+        public int saltClusterSize = 8;
     }
 }
