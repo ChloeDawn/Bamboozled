652dc97143e9f5f63814592cb22cd62a19f35729
InsomniaKitten <admin@designdevils.co.uk>
2018-08-08 14:27:00 +0100

    Update EntityRendererThrownSaltPile

--- src/main/java/net/insomniakitten/bamboo/client/render/EntityRendererThrownSaltPile.java
+++ src/main/java/net/insomniakitten/bamboo/client/render/EntityRendererThrownSaltPile.java
@@ -5,7 +5,7 @@ import net.insomniakitten.bamboo.BamboozledItems;
 import net.insomniakitten.bamboo.entity.EntityThrownSaltPile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureMap;
@@ -16,6 +16,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public final class EntityRendererThrownSaltPile extends Render<EntityThrownSaltPile> {
+    private final ItemStack stack = new ItemStack(BamboozledItems.SALT_PILE);
+
     public EntityRendererThrownSaltPile(final RenderManager manager) {
         super(manager);
     }
@@ -28,6 +30,7 @@ public final class EntityRendererThrownSaltPile extends Render<EntityThrownSaltP
         GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
         GlStateManager.rotate(this.getRotationAngle(), 1.0F, 0.0F, 0.0F);
         GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+
         this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 
         if (this.renderOutlines) {
@@ -35,10 +38,7 @@ public final class EntityRendererThrownSaltPile extends Render<EntityThrownSaltP
             GlStateManager.enableOutlineMode(this.getTeamColor(entity));
         }
 
-        Minecraft.getMinecraft().getRenderItem().renderItem(
-            new ItemStack(BamboozledItems.SALT_PILE),
-            ItemCameraTransforms.TransformType.GROUND
-        );
+        Minecraft.getMinecraft().getRenderItem().renderItem(this.stack, TransformType.GROUND);
 
         if (this.renderOutlines) {
             GlStateManager.disableOutlineMode();
@@ -57,8 +57,8 @@ public final class EntityRendererThrownSaltPile extends Render<EntityThrownSaltP
     }
 
     private float getRotationAngle() {
-        val thirdPerson = this.renderManager.options.thirdPersonView;
-        val viewX = this.renderManager.playerViewX;
-        return (float) (thirdPerson == 2 ? -1 : 1) * viewX;
+        val thirdPersonView = this.renderManager.options.thirdPersonView;
+        val playerViewX = this.renderManager.playerViewX;
+        return (float) (thirdPersonView == 2 ? -1 : 1) * playerViewX;
     }
 }
