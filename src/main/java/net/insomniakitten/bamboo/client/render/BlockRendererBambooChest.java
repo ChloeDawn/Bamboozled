6926b17b141b3e58124ea418e0706384797eb000
InsomniaKitten <admin@designdevils.co.uk>
2018-08-08 15:46:04 +0100

    Update BlockRendererBambooChest

--- src/main/java/net/insomniakitten/bamboo/client/render/BlockRendererBambooChest.java
+++ src/main/java/net/insomniakitten/bamboo/client/render/BlockRendererBambooChest.java
@@ -1,5 +1,6 @@
 package net.insomniakitten.bamboo.client.render;
 
+import lombok.experimental.var;
 import lombok.val;
 import net.insomniakitten.bamboo.block.BlockBambooChest;
 import net.insomniakitten.bamboo.block.entity.BlockEntityBambooChest;
@@ -8,6 +9,7 @@ import net.minecraft.client.model.ModelChest;
 import net.minecraft.client.model.ModelLargeChest;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -42,138 +44,122 @@ public final class BlockRendererBambooChest extends TileEntitySpecialRenderer<Bl
         GlStateManager.depthFunc(GL11.GL_LEQUAL);
         GlStateManager.depthMask(true);
 
-        int meta = 0;
+        var facing = EnumFacing.SOUTH;
 
-        if (chest.hasWorld()) {
-            val block = chest.getBlockType();
-            meta = chest.getBlockMetadata();
-
-            if (block instanceof BlockChest && meta == 0) {
-                val world = chest.getWorld();
-                val pos = chest.getPos();
-                val state = world.getBlockState(pos);
-                ((BlockChest) block).checkForSurroundingChests(world, pos, state);
-                meta = chest.getBlockMetadata();
-            }
-
-            chest.checkForAdjacentChests();
+        if (chest.hasWorld() && chest.getBlockType() instanceof BlockBambooChest) {
+            val world = chest.getWorld();
+            val pos = chest.getPos();
+            val state = world.getBlockState(pos);
+            facing = state.getValue(BlockChest.FACING);
         }
 
-        if (chest.adjacentChestZNeg == null && chest.adjacentChestXNeg == null) {
-            ModelChest chestModel;
+        if (chest.adjacentChestZNeg != null || chest.adjacentChestXNeg != null) {
+            return;
+        }
 
-            if (chest.adjacentChestXPos == null && chest.adjacentChestZPos == null) {
-                chestModel = this.chestModel;
+        val chestModel = this.isDouble(chest) ? this.doubleChestModel : this.chestModel;
 
-                if (destroyStage < 0) {
-                    if (this.isChristmas) {
-                        this.bindTexture(BlockRendererBambooChest.TEX_CHRISTMAS);
-                    } else if (chest.getChestType() == BlockBambooChest.TYPE_BAMBOO_TRAP) {
-                        this.bindTexture(BlockRendererBambooChest.TEX_TRAPPED);
-                    } else {
-                        this.bindTexture(BlockRendererBambooChest.TEX_NORMAL);
-                    }
-                } else {
-                    this.bindTexture(TileEntitySpecialRenderer.DESTROY_STAGES[destroyStage]);
-                    GlStateManager.matrixMode(GL11.GL_TEXTURE);
-                    GlStateManager.pushMatrix();
-                    GlStateManager.scale(4.0F, 4.0F, 1.0F);
-                    GlStateManager.translate(0.0625F, 0.0625F, 0.0625F);
-                    GlStateManager.matrixMode(GL11.GL_MODELVIEW);
-                }
-            } else {
-                chestModel = this.doubleChestModel;
-
-                if (destroyStage < 0) {
-                    if (this.isChristmas) {
-                        this.bindTexture(BlockRendererBambooChest.TEX_CHRISTMAS_DOUBLE);
-                    } else if (chest.getChestType() == BlockBambooChest.TYPE_BAMBOO_TRAP) {
-                        this.bindTexture(BlockRendererBambooChest.TEX_TRAPPED_DOUBLE);
-                    } else {
-                        this.bindTexture(BlockRendererBambooChest.TEX_NORMAL_DOUBLE);
-                    }
-                } else {
-                    this.bindTexture(TileEntitySpecialRenderer.DESTROY_STAGES[destroyStage]);
-                    GlStateManager.matrixMode(GL11.GL_TEXTURE);
-                    GlStateManager.pushMatrix();
-                    GlStateManager.scale(8.0F, 4.0F, 1.0F);
-                    GlStateManager.translate(0.0625F, 0.0625F, 0.0625F);
-                    GlStateManager.matrixMode(GL11.GL_MODELVIEW);
-                }
-            }
-
+        if (destroyStage < 0) {
+            this.bindTexture(this.getTexture(chest));
+        } else {
+            this.bindTexture(this.getDestroyStage(destroyStage));
+            GlStateManager.matrixMode(GL11.GL_TEXTURE);
             GlStateManager.pushMatrix();
-            GlStateManager.enableRescaleNormal();
+            GlStateManager.scale(this.isDouble(chest) ? 8.0F : 4.0F, 4.0F, 1.0F);
+            GlStateManager.translate(0.0625F, 0.0625F, 0.0625F);
+            GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        }
 
-            if (destroyStage < 0) {
-                GlStateManager.color(1.0F, 1.0F, 1.0F, alpha);
-            }
+        GlStateManager.pushMatrix();
+        GlStateManager.enableRescaleNormal();
 
-            GlStateManager.translate((float) x, (float) y + 1.0F, (float) z + 1.0F);
-            GlStateManager.scale(1.0F, -1.0F, -1.0F);
-            GlStateManager.translate(0.5F, 0.5F, 0.5F);
+        if (destroyStage < 0) {
+            GlStateManager.color(1.0F, 1.0F, 1.0F, alpha);
+        }
 
-            int rotationAngle = 0;
+        GlStateManager.translate((float) x, (float) y + 1.0F, (float) z + 1.0F);
+        GlStateManager.scale(1.0F, -1.0F, -1.0F);
+        GlStateManager.translate(0.5F, 0.5F, 0.5F);
 
-            if (meta == 2) {
-                rotationAngle = 180;
-            }
+        if (EnumFacing.NORTH == facing && chest.adjacentChestXPos != null) {
+            GlStateManager.translate(1.0F, 0.0F, 0.0F);
+        }
 
-            if (meta == 4) {
-                rotationAngle = 90;
-            }
+        if (EnumFacing.EAST == facing && chest.adjacentChestZPos != null) {
+            GlStateManager.translate(0.0F, 0.0F, -1.0F);
+        }
 
-            if (meta == 5) {
-                rotationAngle = -90;
-            }
+        GlStateManager.rotate(facing.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
+        GlStateManager.translate(-0.5F, -0.5F, -0.5F);
 
-            if (meta == 2 && chest.adjacentChestXPos != null) {
-                GlStateManager.translate(1.0F, 0.0F, 0.0F);
-            }
+        var lidAngle = chest.prevLidAngle + (chest.lidAngle - chest.prevLidAngle) * partialTicks;
 
-            if (meta == 5 && chest.adjacentChestZPos != null) {
-                GlStateManager.translate(0.0F, 0.0F, -1.0F);
-            }
+        val adjZ = chest.adjacentChestZNeg;
+        val adjX = chest.adjacentChestXNeg;
 
-            GlStateManager.rotate((float) rotationAngle, 0.0F, 1.0F, 0.0F);
-            GlStateManager.translate(-0.5F, -0.5F, -0.5F);
+        if (adjZ != null) {
+            var adjZLidAngle = adjZ.prevLidAngle + (adjZ.lidAngle - adjZ.prevLidAngle) * partialTicks;
 
-            float lidAngle = chest.prevLidAngle + (chest.lidAngle - chest.prevLidAngle) * partialTicks;
-
-            val adjZ = chest.adjacentChestZNeg;
-            val adjX = chest.adjacentChestXNeg;
-
-            if (adjZ != null) {
-                float adjZLidAngle = adjZ.prevLidAngle + (adjZ.lidAngle - adjZ.prevLidAngle) * partialTicks;
-
-                if (adjZLidAngle > lidAngle) {
-                    lidAngle = adjZLidAngle;
-                }
-            }
-
-            if (adjX != null) {
-                float adjXLigAngle = adjX.prevLidAngle + (adjX.lidAngle - adjX.prevLidAngle) * partialTicks;
-
-                if (adjXLigAngle > lidAngle) {
-                    lidAngle = adjXLigAngle;
-                }
-            }
-
-            lidAngle = 1.0F - lidAngle;
-            lidAngle = 1.0F - lidAngle * lidAngle * lidAngle;
-
-            chestModel.chestLid.rotateAngleX = -(lidAngle * ((float) Math.PI / 2.0F));
-            chestModel.renderAll();
-
-            GlStateManager.disableRescaleNormal();
-            GlStateManager.popMatrix();
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
-            if (destroyStage >= 0) {
-                GlStateManager.matrixMode(GL11.GL_TEXTURE);
-                GlStateManager.popMatrix();
-                GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+            if (adjZLidAngle > lidAngle) {
+                lidAngle = adjZLidAngle;
             }
         }
+
+        if (adjX != null) {
+            var adjXLigAngle = adjX.prevLidAngle + (adjX.lidAngle - adjX.prevLidAngle) * partialTicks;
+
+            if (adjXLigAngle > lidAngle) {
+                lidAngle = adjXLigAngle;
+            }
+        }
+
+        lidAngle = 1.0F - lidAngle;
+        lidAngle = 1.0F - lidAngle * lidAngle * lidAngle;
+
+        chestModel.chestLid.rotateAngleX = -(lidAngle * ((float) Math.PI / 2.0F));
+        chestModel.renderAll();
+
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.popMatrix();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+        if (destroyStage >= 0) {
+            GlStateManager.matrixMode(GL11.GL_TEXTURE);
+            GlStateManager.popMatrix();
+            GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        }
+    }
+
+    private boolean isDouble(final BlockEntityBambooChest chest) {
+        return chest.adjacentChestZPos != null || chest.adjacentChestXPos != null;
+    }
+
+    private boolean isTrapped(final BlockEntityBambooChest chest) {
+        return BlockBambooChest.TYPE_BAMBOO_TRAP == chest.getChestType();
+    }
+
+    private ResourceLocation getTexture(final BlockEntityBambooChest chest) {
+        if (this.isDouble(chest)) {
+            if (this.isChristmas) {
+                return BlockRendererBambooChest.TEX_CHRISTMAS_DOUBLE;
+            } else if (this.isTrapped(chest)) {
+                return BlockRendererBambooChest.TEX_TRAPPED_DOUBLE;
+            } else {
+                return BlockRendererBambooChest.TEX_NORMAL_DOUBLE;
+            }
+        } else {
+            if (this.isChristmas) {
+                return BlockRendererBambooChest.TEX_CHRISTMAS;
+            } else if (this.isTrapped(chest)) {
+                return BlockRendererBambooChest.TEX_TRAPPED;
+            } else {
+                return BlockRendererBambooChest.TEX_NORMAL;
+            }
+        }
+    }
+
+    private ResourceLocation getDestroyStage(final int destroyStage) {
+        val stages = TileEntitySpecialRenderer.DESTROY_STAGES;
+        return stages[destroyStage % stages.length];
     }
 }
